b06b9f9bcdb2a7c47c526bf50d6f8ac3d5038786
Attben <72492246+Attben@users.noreply.github.com>
2021-05-19 15:34:48 +0200

    Fixed crash in PlayMenu.StartGame()

--- LudoLike/Dice.cs
+++ LudoLike/Dice.cs
@@ -12,5 +12,6 @@ namespace LudoLike
         public int AnimationTimer { get; set; } = 0;
         public static readonly CanvasBitmap[] DiceImages = new CanvasBitmap[6];
         public static CanvasBitmap SpinningDieImage;
+
     }
 }
--- LudoLike/Game.cs
+++ LudoLike/Game.cs
@@ -11,7 +11,14 @@ namespace LudoLike
         private List<Player> _players;
         private List<Tile> _board;
 
-
+        public Game(int numberOfPlayers)
+        {
+            _players = new List<Player>(numberOfPlayers);
+            for (int i = 0; i < numberOfPlayers; i++)
+            {
+                _players.Add(new Player());
+            }
+        }
 
         public void AddPlayers(int amount)
         {
