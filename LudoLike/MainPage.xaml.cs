2d4ee50a5ff9539ca5a8d23b40656ed06d50d128
Attben <72492246+Attben@users.noreply.github.com>
2021-05-19 15:33:11 +0200

    Moved a few scaling methods into the class "Scaling".

--- LudoLike/MainPage.xaml.cs
+++ LudoLike/MainPage.xaml.cs
@@ -30,22 +30,19 @@ namespace LudoLike
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        public double bWidth = ApplicationView.GetForCurrentView().VisibleBounds.Width;
-        public double bHeight = ApplicationView.GetForCurrentView().VisibleBounds.Height;
-        public static float scaleWidth, scaleHeight;
-        public static int DesignWidth = 1920;
-        public static int DesignHeight = 1080;
-
         public CanvasBitmap BG;
         public CanvasBitmap CurrentDieImage;
 
         private Dice _dice = new Dice();
         private Random _prng = new Random();
+        private Scaling _scaling = new Scaling();
 
         public MainPage()
         {
             this.InitializeComponent();
-            ScalingInit();
+            Window.Current.SizeChanged += CurrentSizeChanged;
+            _scaling.ScalingInit();
+            ControlProperties(_scaling.bWidth, _scaling.bHeight);
         }
 
         private void CanvasCreateResources(
@@ -53,6 +50,12 @@ namespace LudoLike
             CanvasCreateResourcesEventArgs args)
         {
             args.TrackAsyncAction(CreateResourcesAsync(sender).AsAsyncAction());
+        }
+
+        public void ControlProperties(double width, double height)
+        {
+            Canvas.Width = width;
+            Canvas.Height = height;
         }
 
         async Task CreateResourcesAsync(CanvasAnimatedControl sender)
@@ -69,11 +72,17 @@ namespace LudoLike
             CurrentDieImage = Dice.DiceImages[2];
         }
 
+        private void CurrentSizeChanged(object sender, WindowSizeChangedEventArgs e)
+        {
+            Scaling.SetScale(e.Size.Width, e.Size.Height);
+            ControlProperties(e.Size.Width, e.Size.Height);
+        }
+
         private void CanvasDraw(
             ICanvasAnimatedControl sender,
             CanvasAnimatedDrawEventArgs args)
         {
-            args.DrawingSession.DrawImage(TransformImage(BG));
+            args.DrawingSession.DrawImage(Scaling.TransformImage(BG));
             if (_dice.AnimationTimer == 0)
             {
                 args.DrawingSession.DrawImage(CurrentDieImage, 200, 200);
@@ -96,57 +105,12 @@ namespace LudoLike
         {
 
         }
-
-        public void ScalingInit()
-        {
-            SetScale(bWidth, bHeight);
-            ControlProperties(bWidth, bHeight);
-            Window.Current.SizeChanged += CurrentSizeChanged;
-        }
-
-        private void CurrentSizeChanged(object sender, WindowSizeChangedEventArgs e)
-        {
-            SetScale(e.Size.Width, e.Size.Height);
-            ControlProperties(e.Size.Width, e.Size.Height);
-        }
-
-        private void ControlProperties(double width, double height)
-        {
-            Canvas.Width = width;
-            Canvas.Height = height;
-        }
-
         private void RollDie(object sender, RoutedEventArgs e)
         {
             CurrentDieImage = Dice.SpinningDieImage;
             _dice.AnimationTimer = 40;
 
             CurrentDieImage = Dice.DiceImages[_prng.Next(0, 5)];
-        }
-
-        public static void SetScale(double width, double height)
-        {
-            scaleWidth = (float)(width / DesignWidth);
-            scaleHeight = (float)(height / DesignHeight);
-        }
-
-        public static Transform2DEffect TransformImage(CanvasBitmap sourceImage)
-        {
-            Transform2DEffect image = new Transform2DEffect() { Source = sourceImage };
-            image.TransformMatrix = Matrix3x2.CreateScale(scaleWidth, scaleHeight);
-            return image;
-        }
-
-        public static float Xpos(float x)
-        {
-            float output = x * scaleWidth;
-            return output;
-        }
-
-        public static float Ypos(float y)
-        {
-            float output = y * scaleHeight;
-            return output;
         }
     }
 }
