817242a47e79d2d636024c1b8ab22d0d624f6fec
Sebastian <sebastian.jerreling@hotmail.com>
2021-05-28 09:56:15 +0200

    Created uwp unit testing project

---
+++ UnitTestLudoLike/LudoBoardTests.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestLudoLike
+{
+    [TestClass]
+    public class LudoBoardTests
+    {
+        [TestMethod]
+        public void LudoBoardConstructorTest()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+    }
+}
