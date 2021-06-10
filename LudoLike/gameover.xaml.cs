7546260d48cc47310c9bb42930941feb9969111c
Attben <72492246+Attben@users.noreply.github.com>
2021-06-10 15:49:12 +0200

    Stop background music when going back to the main menu.

--- LudoLike/gameover.xaml.cs
+++ LudoLike/gameover.xaml.cs
@@ -69,6 +69,7 @@ namespace LudoLike
         private void Grid_KeyUp(object sender, KeyRoutedEventArgs e)
         {
             Classes.Highscore.AddHighscores(_players);
+            Classes.SoundMixer.StopPlaying(Classes.SoundChannels.music);
             this.Frame.Navigate(typeof(MainMenu));
         }
 
